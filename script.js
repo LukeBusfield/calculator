8f453514a83e24ff2a9e2dbd3658826ebc2fb149
Luke Busfield <luke05busfield@gmail.com>
2023-06-19 22:04:28 +0200

    number, clear and operator button listeners added,

--- script.js
+++ script.js
@@ -38,3 +38,36 @@ function operate(s1, s2, op){
             return divide(s1, s2);         
     }    
 }
+
+const display = document.getElementById("display");
+
+let displayValue = display.innerHTML;
+
+function addButtonListenerNumber(id, value){
+    const btn = document.getElementById(id);
+    btn.addEventListener("click", function(){
+        displayValue += value;
+        display.innerHTML = displayValue;
+    });
+}
+
+addButtonListenerNumber("one", 1);
+addButtonListenerNumber("two", 2);
+addButtonListenerNumber("three", 3);
+addButtonListenerNumber("four", 4);
+addButtonListenerNumber("five", 5);
+addButtonListenerNumber("six", 6);
+addButtonListenerNumber("seven", 7);
+addButtonListenerNumber("eight", 8);
+addButtonListenerNumber("nine", 9);
+addButtonListenerNumber("zero", 0);
+addButtonListenerNumber("plus", "+");
+addButtonListenerNumber("minus", "-");
+addButtonListenerNumber("multiply", "*");
+addButtonListenerNumber("divide", "/");
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", function(){
+    displayValue = "";
+    display.innerHTML = displayValue;
+});
